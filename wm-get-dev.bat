b31c173fe9fece1589eba2f3e71a5bad2c493eea
BPS <kadokura.souji@gmail.com>
2026-06-05 19:20:45 +0900

    ディレクトリ構築用のbat

---
+++ wm-get-dev.bat
@@ -0,0 +1,26 @@
+ehco　"wm-getを作る"
+mkdir wm-get
+ehco "wm-getへ移動"
+cd wm-get
+ehco "repo-lost pkg libを作る"
+mkdir repo-list
+mkdir pkg
+mkdir lib
+ehco "pkgへ移動"
+cd pkg
+ehco "zpkg lpkgを作る"
+mkdir zpkg
+mkdir lpkg
+ehco "wm-getへ移動"
+cd ../
+ehco "libへ移動"
+cd lib
+ehco "wmg-lib bat-libを作る"
+mkdir wmg-lib
+mkdir bat-lib
+ehco "wmg-libへ移動"
+cd wmg-lib
+ehco "7zip wgetceを作る"
+mkdir 7zip
+mkdir wgetce
+dir
